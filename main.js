e228ef07ec94bdd4895103b202862ed99ed15fc9
Samuel Uzelino <uzelino.sam@gmail.com>
2025-05-15 17:58:01 +0200

    Add weather icons display in showForecast function for hourly forecast

--- main.js
+++ main.js
@@ -55,7 +55,13 @@ async function showForecast(latlng) {
         </ul>
         `;
   
+    //Wettericons für die nächsten 24 Stunden in 3 Stunden Schritten
+    for (let i = 0; i <= 24; i+=3) {
+        let symbol = jsondata.properties.timeseries[i].data.next_1_hours.summary.symbol_code;
+        console.log(symbol);
+    }
 
+        
     L.popup([
         latlng.lat, latlng.lng
     ], {
